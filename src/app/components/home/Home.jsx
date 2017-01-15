a823fa397a55638fe480dc5553cfaf1450809dfb
alex rodriguez <alex.rodriguez@austintexas.gov>
2017-01-15 17:11:22 -0600

    removed counter from home component

--- src/app/components/home/Home.jsx
+++ src/app/components/home/Home.jsx
@@ -8,29 +8,15 @@ class Home extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      count: 0,
       textInputValue: '',
       textAreaValue: '',
       selectedValue: '',
     }
-    this.onIncrement = this.onIncrement.bind(this)
-    this.onDecrement = this.onDecrement.bind(this)
     this.handleTextInput = this.handleTextInput.bind(this)
     this.handleTextArea = this.handleTextArea.bind(this)
     this.handleSelect = this.handleSelect.bind(this)
   }
 
-  onIncrement () {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
-  onDecrement ()  {
-    this.setState({
-      count: this.state.count - 1
-    })
-  }
 
   handleTextInput (e) {
     this.setState({
@@ -53,10 +39,8 @@ class Home extends React.Component {
   render () {
     return (
       <div className="container home">
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.onIncrement}>+</button>
-        <button onClick={this.onDecrement}>-</button>
-        <br />
+        <h1>Form element components:</h1>
+
         <form>
           <TextInput 
             label={'To Do'}
